e9b947bc7684d58a984686b4add53f7d87db2362
XuTongle <xutongle@msn.com>
2021-08-11 11:44:23 +0800

    c

--- src/helpers.php
+++ src/helpers.php
@@ -4,3 +4,29 @@
  * @copyright Copyright (c) 2010-2099 Jinan JiYuan Information Technology Co., Ltd.
  * @link https://www.yaoqiyuan.com/
  */
+
+use Hyperf\Utils\ApplicationContext;
+use Larva\Settings\SettingsRepository;
+
+if (!function_exists('setting')) {
+    /**
+     * Get setting value or object.
+     *
+     * @param string $key
+     * @param mixed|null $default
+     * @return SettingsRepository|mixed
+     */
+    function setting(string $key = '', $default = null)
+    {
+        if (!ApplicationContext::hasContainer()) {
+            throw new \RuntimeException('The application context lacks the container.');
+        }
+
+        $store = ApplicationContext::getContainer()->get(SettingsRepository::class);
+
+        if (empty($key)) {
+            return $store;
+        }
+        return $store->get($key, $default);
+    }
+}
